436c6e21eac3fa9a2a4d6c71cae1500c72c60742
Alim Kerem Erdoğmuş <kerem.erdogmus@inovasyonmuhendislik.com>
2022-05-17 09:48:21 +0300

    Add files via upload

---
+++ manuscript_word_counter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#fname = input("Enter file name: ")
+fname ="/home/ros/manuscript.txt" 
+num_words = 0
+ 
+with open(fname, 'r') as f:
+    for line in f:
+        words = line.split()
+        num_words += len(words)
+print("Number of words:")
+print(num_words)
